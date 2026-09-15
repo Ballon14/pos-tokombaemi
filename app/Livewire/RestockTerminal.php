bee59a09507319f99dcbfc61219f8b6714dfc169
Ballon14 <akungithub1465@gmail.com>
2026-09-15 23:48:28 +0700

    Fix search 500 error: remove non-existent barcode column from query

--- app/Livewire/RestockTerminal.php
+++ app/Livewire/RestockTerminal.php
@@ -274,7 +274,6 @@ class RestockTerminal extends Component
             $query->where(function ($q) {
                 $q->where('name', 'like', '%'.$this->search.'%')
                     ->orWhere('sku', 'like', '%'.$this->search.'%')
-                    ->orWhere('barcode', $this->search)
                     ->orWhereHas('category', function ($q2) {
                         $q2->where('name', 'like', '%'.$this->search.'%');
                     });
